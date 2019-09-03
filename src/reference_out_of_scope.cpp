5c9a2ced38e5e79cd8862a30c5cff4b50753fcd7
Geoffrey Viola <geoffviola@users.noreply.github.com>
2019-09-02 23:43:00 -0700

    add refrence out of scope

---
+++ src/reference_out_of_scope.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+
+int& get_bad_reference() {
+  int a = 1;
+  return a;
+}
+
+int main() {
+  const int& a = get_bad_reference();
+  std::cout << "a=" << a << '\n';
+  return EXIT_SUCCESS;
+}
+
