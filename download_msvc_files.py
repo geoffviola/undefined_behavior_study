59c80ed12217b0d828a2548f498a74e66c7955d0
Geoffrey Viola <geoffviola@users.noreply.github.com>
2021-07-31 19:59:08 -0700

    update msvc log

--- download_msvc_files.py
+++ download_msvc_files.py
@@ -17,7 +17,7 @@ def parse_args():
     parser.add_argument('--output_runtime_relwithdebinfo', type=str,
                         default="rel_with_deb_info/results.txt")
     parser.add_argument('--text_url', type=str,
-                        default="https://ci.appveyor.com/api/buildjobs/it2kqk78ofxsrv7x/log",
+                        default="https://ci.appveyor.com/api/buildjobs/qq9cd1p4qr6y241i/log",
                         help='msvc text file')
     return parser.parse_args()
 
