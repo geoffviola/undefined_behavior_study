f8ff214c29f688b03b47515cc9e9f767ebebf9ef
geoffviola <geoffviola@users.noreply.github.com>
2024-07-06 17:48:51 -0400

    add reinterpret_cast files

---
+++ src/reinterpret_cast_unaligned_bytes_to_uint32.cpp
@@ -0,0 +1,26 @@
+/*
+https://eel.is/c++draft/expr#basic.lval-11
+7.2.1.11
+#
+If a program attempts to access ([defns.access]) the stored value of an object
+through a glvalue whose type is not similar ([conv.qual]) to one of the
+following types the behavior is undefined:52
+
+    (11.1)
+    the dynamic type of the object,
+    (11.2)
+    a type that is the signed or unsigned type corresponding to the dynamic type
+of the object, or (11.3) a char, unsigned char, or std​::​byte type.
+*/
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+  const std::array<std::byte, 4> a{};
+  std::cout << *reinterpret_cast<const uint32_t *>(&a) << '\n';
+  return EXIT_SUCCESS;
+}
